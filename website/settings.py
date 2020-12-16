24255f573837d41e3ce2b487b6e6fee995c01627
edmund-project <76051071+edmund-project@users.noreply.github.com>
2020-12-16 12:47:07 +0700

    Add files via upload

--- website/settings.py
+++ website/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
